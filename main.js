44ff1c4162cccb86e4ecc464728c34da3bdf317f
joseph sul <joesul7@gmail.com>
2016-07-28 00:47:19 -0400

    changes

--- main.js
+++ main.js
@@ -202,8 +202,8 @@ console.log("J-E-T-S! JETS! JETS! JETS!");
 
   function deleteFaves() {
     $.ajax({
-      // url: localHostUrl + '/jmc/favorites',
-      url: herokuURL + '/jmc/favorites',
+      // url: localHostUrl + '/jmc/delete',
+      url: herokuURL + '/jmc/delete',
       method: 'DELETE',
       dataType: 'json'
     }).done(function(response){
